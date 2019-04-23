5e7144673c0a2224e3c8ddc6d7872664c9d02538
SkiingKing <45917637+SkiingKing@users.noreply.github.com>
2019-04-23 21:34:16 +0300

    TODO do scrollview

--- app/src/main/java/com/example/user/third_ekran/MainActivity.java
+++ app/src/main/java/com/example/user/third_ekran/MainActivity.java
@@ -1,60 +1,135 @@
 package com.example.user.third_ekran;
 
-import android.support.v7.app.AppCompatActivity;
+
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
-
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.view.LineChartView;
+import lecho.lib.hellocharts.view.PieChartView;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
+
+
     TextView advise;
     ArrayList<Integer> result = new ArrayList<Integer>();
-    ArrayList<Integer> data = new ArrayList<Integer>();
-
+    ArrayList<Integer> datae = new ArrayList<Integer>();
+     LineChartView lineChartView;
+    PieChartView pieChartView;
+    ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        result.add(0,8);
-        result.add(1,10);
-        result.add(2,15);
-        result.add(3,20);
-        result.add(4,22);
 
 
-        data.add(0,11);
-        data.add(1,11);
-        data.add(2,12);
-        data.add(3,13);
-        data.add(4,14);
+        result.add(0, 34);
+        result.add(1, 20);
+        result.add(2, 30);
+        result.add(3, 25);
+        result.add(4, 20);
+
+
+        datae.add(0, 1);
+        datae.add(1, 2);
+        datae.add(2, 3);
+        datae.add(3, 4);
+        datae.add(4, 5);
 
 
         TextView advise = findViewById(R.id.advise_1);
 
 
-        GraphView graph = findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
-                new DataPoint(result.get(0), data.get(0)),
-                new DataPoint(result.get(1), data.get(1)),
-                new DataPoint(result.get(2), data.get(2)),
-                new DataPoint(result.get(3), data.get(3)),
-                new DataPoint(result.get(4), data.get(4))
 
-        });
-        graph.addSeries(series);
+        lineChartView = findViewById(R.id.chart);
 
-        for(int i=0;i<result.size();i++){
-            if(result.get(i) <= 10) advise.setText(R.string.advise_1);
+
+        List yAxisValues = new ArrayList();
+        List axisValues = new ArrayList();
+
+
+        Line line = new Line(yAxisValues).setColor(getResources().getColor(R.color.gold));
+
+        for (int i = 0; i < datae.size(); i++) {
+            axisValues.add(i, new AxisValue(i).setLabel(String.valueOf(datae.get(i))));
+        }
+
+        for (int i = 0; i < result.size(); i++) {
+            yAxisValues.add(new PointValue(i, result.get(i)));
+        }
+
+        List lines = new ArrayList();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        Axis axis = new Axis();
+        axis.setValues(axisValues);
+        axis.setTextSize(16);
+        axis.setName(getResources().getString(R.string.data));
+        axis.setTextColor(getResources().getColor(R.color.black));
+        data.setAxisXBottom(axis);
+
+        Axis yAxis = new Axis();
+        yAxis.setName(getResources().getString(R.string.result));
+        yAxis.setTextColor(getResources().getColor(R.color.black));
+        yAxis.setTextSize(16);
+        data.setAxisYLeft(yAxis);
+
+        lineChartView.setLineChartData(data);
+        Viewport viewport = new Viewport(lineChartView.getMaximumViewport());
+        viewport.top = 110;
+        lineChartView.setMaximumViewport(viewport);
+        lineChartView.setCurrentViewport(viewport);
+
+
+
+        pieChartView = findViewById(R.id.pieChartView);
+
+        List pieData = new ArrayList<>();
+        pieData.add(new SliceValue(15, getColor(R.color.redd)).setLabel("Math"));
+        pieData.add(new SliceValue(25,  getColor(R.color.pieData_second_color)).setLabel("English"));
+        pieData.add(new SliceValue(10,  getColor(R.color.pieData_third_color)).setLabel("Physics"));
+
+
+        PieChartData pieChartData = new PieChartData(pieData);
+        pieChartData.setHasLabels(true).setValueLabelTextSize(14);
+        pieChartData.setCenterText1(getResources().getString(R.string.subjects));
+        pieChartData.setHasCenterCircle(true).setCenterText1FontSize(20).setCenterText1Color(Color.BLACK);
+        pieChartView.setPieChartData(pieChartData);
+
+
+
+
+
+
+        for (int i = 0; i < result.size(); i++) {
+            if (result.get(i) <= 10) advise.setText(R.string.advise_1);
             else advise.setText(R.string.advise_1_1);
         }
 
     }
+
+
 }
